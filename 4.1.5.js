d7387656c6a3da641c7fb52f43ade87c6ffda7a0
Grekos89 <70657407+Grekos89@users.noreply.github.com>
2020-09-12 14:52:42 +0300

    Create 4.1.5.js

---
+++ 4.1.5.js
@@ -0,0 +1,16 @@
+<script>
+  let menu = {
+  width: 200,
+  height: 300,
+  title: "My menu"
+};
+
+  function multiplyNumeric(menu) {
+    for (let key in menu) {
+      if (typeof menu[key] == 'number') {
+        menu[key] *= 2;
+      }
+    }
+  }
+
+</script>
